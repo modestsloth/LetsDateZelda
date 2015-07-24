57a12c2fccbdb07e04b03e1a43654bb315a07b89
moophyre <jjackson40@angelo.edu>
2015-07-24 16:30:29 -0500

    Added mutiple functions for the Character struct I created, allows for manipulation and assignment of the initial character. Also added to the BATTLE state, has a mock simulation of runninginto the dungroun against a monster, just does basic attacks for now and outputs the putcome. Added a cheap function to simulate a clear for the console, it just adds a lot of newlines. Everything compiles and runs, you are able to see the stats in the exam window, also you can explore the dungeon and find encounters

--- LetsDateZelda/LetsDateZelda.cpp
+++ LetsDateZelda/LetsDateZelda.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <Windows.h>
 #include <vector>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -56,7 +57,159 @@ struct Room
 	Room *up = NULL;
 };
 
+//The struct for the characters that will be initialized
+/*
+	Stats that will be included in this area.
+	-Health
+	-Strength
+	-Name
+	-Intelligence
+	-Happiness
+	-Experience
+	-Level
+*/
 
+struct Character
+{
+	int health;
+	int strength;
+	int intelligence;
+	int happiness;
+	int exp;
+	int lvl;
+	
+	string name;
+};
+
+void modHealth(Character &mainToon, int mod);
+void modStr(Character &mainToon, int mod);
+void modInt(Character &mainToon, int mod);
+void modHapp(Character &mainToon, int mod);
+void modLvl(Character &mainToon, int mod);
+void modExp(Character &mainToon, int mod);
+void battleText(Character &mainToon, STATE &gamestate, int monster);
+void op(string s);
+void dot();
+void ClearScreen();
+/*
+This fucntion is used to initialize the struct Character with the initial values on a new game creation
+--Needs to work, for some reason the variables are not being initialized here
+*/
+void initChar(Character mainToon) {
+	mainToon.health = 100;
+	mainToon.strength = 10;
+	mainToon.intelligence = 10;
+	mainToon.happiness = 0;
+	mainToon.exp = 0;
+	mainToon.lvl = 1;
+	mainToon.name = "Zelda";
+}
+/*
+	This is the funtion that will be used to modify the health of the charcters
+	Will also inlude funtions for modifying
+	-Strngeth
+	-happiness
+	-Intelligence
+	-ect
+*/
+void modHealth(Character &mainToon, int mod) {
+	mainToon.health += mod;
+
+	if (mainToon.health > 100) {
+		mainToon.health = 100;
+	}
+}
+void modStr(Character &mainToon, int mod) {
+	mainToon.strength += mod;
+}
+void modInt(Character &mainToon, int mod) {
+	mainToon.intelligence += mod;
+}
+void modHapp(Character &mainToon, int mod) {
+	mainToon.happiness += mod;
+}
+void modLvl(Character &mainToon, int mod) {
+	mainToon.lvl += mod;
+}
+void modExp(Character &mainToon, int mod) {
+	mainToon.exp += mod;
+
+	if (mainToon.exp >= 100) {
+		modLvl(mainToon, 1);
+	}
+}
+
+/*
+	This function is to hold the battle Text for when an encounter happens
+*/
+void battleText(Character &mainToon, STATE &gamestate,int monster) {
+
+	int hit = rand() % 10 + 1;
+	int dmg = 2;
+	int monHealth = 100;
+	boolean cont = true;
+
+	while (cont) {
+		//SEt the correct dmg ratio based on what monster is fighting
+		if (monster == 1) {
+			dmg = rand() % 5 + 1;
+			if (dmg > 3) {
+				op("ZELDA: The Beast landed a critical hit!");
+				dot();
+			}
+		}
+		else if (monster == 2) {
+			dmg = rand() % 10 + 1;
+			if (dmg > 6) {
+				op("ZELDA: The Beast landed a critical hit!");
+				dot();
+			}
+		}
+		else {
+			dmg = rand() % 15 + 1;
+			if (dmg > 10) {
+				op("ZELDA: The Beast landed a critical hit!");
+				dot();
+			}
+		}
+
+		if (hit > 5) {
+			op("ZELDA: We landed a critical hit Master!");
+			dot();
+		}
+		cout << "You recieved " << dmg << " damage from the enemy" << endl;							cout << "You recieved " << dmg << " damage from the enemy" << endl;
+		cout << "You dealt " << hit << " damage to the enemy" << endl;
+		dot();
+		modHealth(mainToon, -dmg);
+		monHealth -= hit;
+
+		op("ZELDA: I have taken a bit of damage Master!");
+		cout << "Zelda has " << mainToon.health << " life left" << endl;
+		dot();
+
+		if (mainToon.health <= 0) {
+			op("ZELDA: This is the farthest I can go Master");
+			dot();
+			mainToon.health = 100;
+			gamestate = MENU;
+			cont = false;
+		}
+		else if (monHealth <= 0) {
+			op("ZELDA: We have slain the monster Master!");
+			dot();
+			gamestate = BATTLE;
+			cont = false;
+		}
+	}
+}
+
+/*
+	A function used to draw a lot of blank spaces to clear he screen, Temp solution
+*/
+void ClearScreen()
+{
+	cout << string(100, '\n');
+}
 
 void op(string s)
 {	//all output is forwarded here
@@ -79,8 +232,19 @@ void dot()
 	cout << "\n";
 }
 
+
 int main()
 {
+	Character mainToon;
+	//Initialize the new Character struct created
+	mainToon.health = 100;
+	mainToon.strength = 10;
+	mainToon.intelligence = 10;
+	mainToon.happiness = 0;
+	mainToon.exp = 0;
+	mainToon.lvl = 1;
+	mainToon.name = "Zelda";
+
 	//just jump into it
 	string input;
 	op("ZELDA: Hello, My name is Zelda, and I need your help!");
@@ -95,7 +259,6 @@ int main()
 	{
 		if (gamestate == MENU)
 		{
-			
 			op("ZELDA: What would you like to do before we go?");
 			int c = 0;
 			op("1. Go to the dungeon.");
@@ -103,36 +266,35 @@ int main()
 			op("3. Examine your inventory.");
 			op("4. Examine Zelda.");
 			op("5. Talk to Zelda.");
-			do
+
+			cin >> c;
+			switch (c)
 			{
-				cin >> c;
-				switch (c)
-				{
-				case 1:
-					op("ZELDA: Great!, Let's not waste a single minute!");
-					gamestate = EXPLORE;
-					break;
-				case 2:
-					op("ZELDA: Good idea! We can get supplies!");
-					gamestate = STORE;
-					break;
-				case 3:
-					op("You look over your Inventory.");
-					break;
-				case 4:
-					op("You look over Zelda.");
-					gamestate = EXAMINE;
-					break;
-				case 5:
-					op("Speak with Zelda.");
-					gamestate = INTERACT;
-					break;
-				default:
-					break;
-				}
-			} while (c != 1 && c != 2 && c != 3 && c != 4 && c != 5);
-			
+			case 1:
+				op("ZELDA: Great!, Let's not waste a single minute!");
+				gamestate = EXPLORE;
+				break;
+			case 2:
+				op("ZELDA: Good idea! We can get supplies!");
+				gamestate = STORE;
+				break;
+			case 3:
+				op("You look over your Inventory.");
+				break;
+			case 4:
+				ClearScreen();
+				op("You look over Zelda.");
+				gamestate = EXAMINE;
+				break;
+			case 5:
+				op("Speak with Zelda.");
+				gamestate = INTERACT;
+				break;
+			default:
+				break;
+			}
 		}
+		
 		else if (gamestate == EXPLORE)
 		{
 			dot();
@@ -150,13 +312,81 @@ int main()
 			//set up the map
 			Room *entrance;
 			entrance = new Room;
+			gamestate = BATTLE;
 
+		}
+		
+		else if (gamestate == BATTLE)
+		{
+			//This is to simulate random battle exploration While testing the modification functions created
+
+			/*
+				The random number for Monster will be 
+				Bat = 1
+				wolf = 2
+				bear = 3
+			*/
+			int monster = rand() % 3 + 1;
+
+			op("Boss: Welcome to my domain Pitiful Human!");
+			op("Boss: I hope to see your face before I consume your soul");
+			op("Boss: If you can make it past my minions that is");
+			dot();
+
+			int a = 0;
+			op("Zelda: What should we do Master?");
+			op("1: Explore the dungeon");
+			op("2: Exit the dungeon");
+
+			cin >> a;
+			switch (a)
+			{
+			case 1:
+				//Scenario for a bat encounter
+				if (monster == 1) {
+					op("ZELDA: Master a Bat has appeared");
+					dot();
+					battleText(mainToon, gamestate, monster);	
+				}
+				else if (monster == 2) {
+					op("ZELDA: Master a Wolf has appeared");
+					dot();
+					battleText(mainToon, gamestate,monster);
+				}
+				else {
+					op("ZELDA: Master a Bear has appeared");
+					dot();
+					battleText(mainToon, gamestate,monster);
+				}
+				gamestate = BATTLE;
+				break;
+			case 2:
+				op("ZELDA: We shall return to finish where we left off!");
+				mainToon.health = 100;
+				gamestate = MENU;
+				break;
+			default:
+				break;
+			}
 
 
 		}
-		else if (gamestate == BATTLE)
-		{
+		//Impliment the gamestater that allows for you to monitor the characters stats on screen
+		else if (gamestate == EXAMINE) {
 
+			cout << "Name: " << mainToon.name << endl;
+			cout << "Level: " << mainToon.lvl << endl;
+			cout << "Experience: " << mainToon.exp << endl;
+			cout << "Happiness: " << mainToon.happiness << endl;
+			cout << "----------------" << endl;
+
+			cout << "Health: " << mainToon.health << endl;
+			cout << "Strength: " << mainToon.strength << endl;
+			cout << "Intelligence: " << mainToon.intelligence << endl;
+			cout << "----------------" << endl;
+			cout << endl;
+
+			gamestate = MENU;
 		}
 	}
 	
